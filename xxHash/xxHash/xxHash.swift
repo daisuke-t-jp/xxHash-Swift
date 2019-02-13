b4e76abfc0424351425482beb5497b9ab78ed9b9
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-02-13 19:43:42 +0900

    Add Hash base class

---
+++ xxHash/xxHash/xxHash.swift
@@ -0,0 +1,50 @@
+//
+//  xxHash.swift
+//  xxHash
+//
+//  Created by Daisuke T on 2019/02/13.
+//  Copyright © 2019 xxHash. All rights reserved.
+//
+
+import Foundation
+
+// TODO: base class
+// TODO: hash32
+// TODO: hash64
+// TODO: overload func(string, data)
+// TODO: iOS scheme
+// TODO: tvOS scheme
+// TODO: demo
+
+// TODO: unittest
+// TODO: travis
+
+// TODO: podspec
+// TODO: cocoapods
+
+// TODO: readme
+// TODO: header image
+
+public class xxHash {
+
+	// MARK: - Enum, Const
+	public enum Endian {
+		case Little
+		case Big
+	}
+
+
+	
+	// MARK: - Utility
+	static internal func rotl<T: FixedWidthInteger>(_ x: T, r: Int) -> T {
+		return (x << r) | (x >> (T.bitWidth - r))
+	}
+
+	static public func endian() -> Endian {
+		if CFByteOrderGetCurrent() == Int(CFByteOrderLittleEndian.rawValue) {
+			return Endian.Little
+		}
+		
+		return Endian.Big
+	}
+}
