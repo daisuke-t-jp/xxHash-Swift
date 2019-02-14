94b1442a2f823395f5e0390927357eed8f17a1c1
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-02-14 20:11:03 +0900

    Add xxHash64.finalize()

--- xxHash/xxHash/xxHash64.swift
+++ xxHash/xxHash/xxHash64.swift
@@ -70,3 +70,186 @@ public extension xxHash64 {
 		return h2
 	}
 }
+
+
+
+// MARK: - Private
+public extension xxHash64 {
+	
+	static private func finalize(_ h: UInt64, array: [UInt8], len: Int, endian: xxHash.Endian) -> UInt64 {
+		var index = 0
+		var h2 = h
+		
+		func process1() {
+			h2 ^= UInt64(array[index]) &* prime5
+			index += 1
+			h2 = xxHash.rotl(h2, r: 11) &* prime1
+		}
+		
+		func process4() {
+			h2 ^= UInt64(xxHash.UInt8ArrayToUInt(array, index: index, type: UInt32(0), endian: endian)) &* prime1
+			index += 1
+			h2 = xxHash.rotl(h2, r: 23) &* prime2 &+ prime3
+		}
+
+		func process8() {
+			let k1 = round(0, input: xxHash.UInt8ArrayToUInt(array, index: index, type: UInt64(0), endian: endian))
+			index += 2
+			h2 ^= k1
+			h2 = xxHash.rotl(h2, r: 27) &* prime1 &+ prime4
+		}
+		
+		
+		switch len & 31 {
+		case 24:
+			process8()
+			fallthrough
+			
+		case 16:
+			process8()
+			fallthrough
+			
+		case 8:
+			process8()
+			return avalanche(h2)
+			
+			
+		case 28:
+			process8()
+			fallthrough
+			
+		case 20:
+			process8()
+			fallthrough
+			
+		case 12:
+			process8()
+			fallthrough
+
+		case 4:
+			process4()
+			return avalanche(h2)
+			
+			
+		case 25:
+			process8()
+			fallthrough
+			
+		case 17:
+			process8()
+			fallthrough
+			
+		case 9:
+			process8()
+			process1()
+			return avalanche(h2)
+			
+			
+		case 29:
+			process8()
+			fallthrough
+			
+		case 21:
+			process8()
+			fallthrough
+			
+		case 13:
+			process8()
+			fallthrough
+			
+		case 5:
+			process4()
+			process1()
+			return avalanche(h2)
+
+
+		case 26:
+			process8()
+			fallthrough
+			
+		case 18:
+			process8()
+			fallthrough
+			
+		case 10:
+			process8()
+			process1()
+			process1()
+			return avalanche(h2)
+			
+
+		case 30:
+			process8()
+			fallthrough
+			
+		case 22:
+			process8()
+			fallthrough
+			
+		case 14:
+			process8()
+			fallthrough
+			
+		case 6:
+			process4()
+			process1()
+			process1()
+			return avalanche(h2)
+			
+			
+		case 27:
+			process8()
+			fallthrough
+			
+		case 19:
+			process8()
+			fallthrough
+			
+		case 11:
+			process8()
+			process1()
+			process1()
+			process1()
+			return avalanche(h2)
+
+
+		case 31:
+			process8()
+			fallthrough
+
+		case 23:
+			process8()
+			fallthrough
+			
+		case 15:
+			process8()
+			fallthrough
+
+		case 7:
+			process4()
+			fallthrough
+			
+		case 3:
+			process1()
+			fallthrough
+
+		case 2:
+			process1()
+			fallthrough
+			
+		case 1:
+			process1()
+			fallthrough
+			
+		case 0:
+			return avalanche(h2)
+
+
+		default:
+			break
+		}
+		
+		return 0	// unreachable, but some compilers complain without it
+	}
+	
+}
