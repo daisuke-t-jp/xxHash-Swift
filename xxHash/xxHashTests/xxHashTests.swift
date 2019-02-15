c8df02ed4bc18e8d74253397204448a914a48040
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-02-15 20:10:32 +0900

    Add streaming reset test

--- xxHash/xxHashTests/xxHashTests.swift
+++ xxHash/xxHashTests/xxHashTests.swift
@@ -215,7 +215,19 @@ class xxHashTests: XCTestCase {
 			XCTAssertEqual(xxHash32.hash("123456789ABCDEF123456789ABCDEF12", seed: 0x7fffffff), xxh.digest())
 		}
 	}
-	
+
+	func test32Reset() {
+		let xxh = xxHash32()
+
+		xxh.update("123456789ABCDEF")
+		xxh.reset()
+		XCTAssertEqual(xxh.digest(), xxHash32().digest())
+		
+		xxh.update("123456789ABCDEF")
+		xxh.seed = 0x7fffffff	// Reset when setting seed.
+		XCTAssertEqual(xxh.digest(), xxHash32(0x7fffffff).digest())
+	}
+
 	/*
 	func testPerformance() {
         // This is an example of a performance test case.
