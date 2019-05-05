15e06622f03fbf14f5d3be05502151e36bc14ae5
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-05-05 18:42:39 +0900

    Add XXH3 empty file

---
+++ Sources/xxHash/xxHash3.swift
@@ -0,0 +1,22 @@
+//
+//  xxHash3.swift
+//  xxHash-Swift
+//
+//  Created by Daisuke T on 2019/05/05.
+//  Copyright © 2019 xxHash-Swift. All rights reserved.
+//
+
+import Foundation
+
+/// xxHash3 class
+public class xxHash3 {
+  
+  // MARK: - Enum, Const
+  
+  
+  // MARK: - Property
+  
+  
+  // MARK: - Life cycle
+  
+}
