c99aa1fe3264a68e69b146795905a5cbadd34c95
daisuke-t-jp <daisuke.t.jp@gmail.com>
2019-05-06 22:05:22 +0900

    Update xxHash3.swift

--- Sources/xxHash/xxHash3.swift
+++ Sources/xxHash/xxHash3.swift
@@ -1,23 +1,51 @@
 //
 //  xxHash3.swift
-//  xxHash-Swift
+//  xxHash
 //
-//  Created by Daisuke T on 2019/05/05.
-//  Copyright © 2019 xxHash-Swift. All rights reserved.
+//  Created by Daisuke T on 2019/05/06.
+//  Copyright © 2019 xxHash. All rights reserved.
 //
 
 import Foundation
 
-/// XXH3 class
+/// xxHash3 class
 typealias XXH3 = xxHash3
 public class xxHash3 {
   
-  // MARK: - Enum, Const
+}
+
+
+// MARK: - 64 bit
+extension xxHash3 {
   
+  static public func digest64(_ array: [UInt8], seed: UInt64 = 0) -> UInt64 {
+    return xxHash3.Bit64.digest(array, seed: seed, endian: Common.endian())
+  }
   
-  // MARK: - Property
+  static public func digest64(_ string: String, seed: UInt64 = 0) -> UInt64 {
+    return xxHash3.Bit64.digest(Array(string.utf8), seed: seed, endian: Common.endian())
+  }
   
+  static public func digest64(_ data: Data, seed: UInt64 = 0) -> UInt64 {
+    return xxHash3.Bit64.digest([UInt8](data), seed: seed, endian: Common.endian())
+  }
+
+}
+
+
+// MARK: - 128 bit
+extension xxHash3 {
   
-  // MARK: - Life cycle
+  static public func digest128(_ array: [UInt8], seed: UInt64 = 0) -> [UInt64] {
+    return xxHash3.Bit128.digest(array, seed: seed, endian: Common.endian())
+  }
+  
+  static public func digest128(_ string: String, seed: UInt64 = 0) -> [UInt64] {
+    return xxHash3.Bit128.digest(Array(string.utf8), seed: seed, endian: Common.endian())
+  }
+  
+  static public func digest128(_ data: Data, seed: UInt64 = 0) -> [UInt64] {
+    return xxHash3.Bit128.digest([UInt8](data), seed: seed, endian: Common.endian())
+  }
   
 }
