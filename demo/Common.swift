b85d9c99d281a7be1d4fbc91fd90a054d3431724
Daisuke TONOSAKI <daisuke.t.jp@gmail.com>
2020-09-23 13:41:29 +0900

    Change tab from 2 space to 4 space

--- demo/Common.swift
+++ demo/Common.swift
@@ -11,119 +11,119 @@ import Foundation
 import xxHash_Swift
 
 class Common {
-  
-  // MARK: - One-shot
-  static func oneshotXXH32() {
-    do {
-      let hash = XXH32.digest("123456789ABCDEF")
-      print("XXH32 One-shot -> 0x\(String(format: "%08x", hash))")
+    
+    // MARK: - One-shot
+    static func oneshotXXH32() {
+        do {
+            let hash = XXH32.digest("123456789ABCDEF")
+            print("XXH32 One-shot -> 0x\(String(format: "%08x", hash))")
+        }
+        
+        do {
+            let hash = XXH32.digest("123456789ABCDEF", seed: 0x7fffffff)
+            print("XXH32 One-shot(0x7fffffff) -> 0x\(String(format: "%08x", hash))")
+        }
     }
     
-    do {
-      let hash = XXH32.digest("123456789ABCDEF", seed: 0x7fffffff)
-      print("XXH32 One-shot(0x7fffffff) -> 0x\(String(format: "%08x", hash))")
-    }
-  }
-  
-  static func oneshotXXH64() {
-    do {
-      let hash = XXH64.digest("123456789ABCDEF")
-      print("XXH64 One-shot -> 0x\(String(format: "%016lx", hash))")
-    }
-    
-    do {
-      let hash = XXH64.digest("123456789ABCDEF", seed: 0x000000007fffffff)
-      print("XXH64 One-shot(0x000000007fffffff) -> 0x\(String(format: "%016lx", hash))")
-    }
-  }
-  
-  static func oneshotXXH3_64() {
-    do {
-      let hash = XXH3.digest64("123456789ABCDEF")
-      print("XXH3-64 One-shot -> 0x\(String(format: "%016lx", hash))")
+    static func oneshotXXH64() {
+        do {
+            let hash = XXH64.digest("123456789ABCDEF")
+            print("XXH64 One-shot -> 0x\(String(format: "%016lx", hash))")
+        }
+        
+        do {
+            let hash = XXH64.digest("123456789ABCDEF", seed: 0x000000007fffffff)
+            print("XXH64 One-shot(0x000000007fffffff) -> 0x\(String(format: "%016lx", hash))")
+        }
     }
     
-    do {
-      let hash = XXH3.digest64("123456789ABCDEF", seed: 0x000000007fffffff)
-      print("XXH3-64 One-shot(0x000000007fffffff) -> 0x\(String(format: "%016lx", hash))")
+    static func oneshotXXH3_64() {
+        do {
+            let hash = XXH3.digest64("123456789ABCDEF")
+            print("XXH3-64 One-shot -> 0x\(String(format: "%016lx", hash))")
+        }
+        
+        do {
+            let hash = XXH3.digest64("123456789ABCDEF", seed: 0x000000007fffffff)
+            print("XXH3-64 One-shot(0x000000007fffffff) -> 0x\(String(format: "%016lx", hash))")
+        }
     }
-  }
-  
-  static func oneshotXXH3_128() {
-    do {
-      let hash = XXH3.digest128("123456789ABCDEF")
-      print("XXH3-128 One-shot -> 0x\(String(format: "%016lx%016lx", hash[0], hash[1]))")
+    
+    static func oneshotXXH3_128() {
+        do {
+            let hash = XXH3.digest128("123456789ABCDEF")
+            print("XXH3-128 One-shot -> 0x\(String(format: "%016lx%016lx", hash[0], hash[1]))")
+        }
+        
+        do {
+            let hash = XXH3.digest128("123456789ABCDEF", seed: 0x000000007fffffff)
+            print("XXH3-128 One-shot(0x000000007fffffff) -> 0x\(String(format: "%016lx%016lx", hash[0], hash[1]))")
+        }
     }
     
-    do {
-      let hash = XXH3.digest128("123456789ABCDEF", seed: 0x000000007fffffff)
-      print("XXH3-128 One-shot(0x000000007fffffff) -> 0x\(String(format: "%016lx%016lx", hash[0], hash[1]))")
+    
+    // MARK: - Streaming
+    static func streamingXXH32() {
+        // Create xxHash instance
+        let xxh = XXH32() // if using seed, e.g. "XXH32(0x7fffffff)"
+        
+        // Get data from file
+        let bundle = Bundle(for: Common.self)
+        let path = bundle.path(forResource: "alice29", ofType: "txt")!
+        let data = NSData(contentsOfFile: path)! as Data
+        
+        let bufSize = 1024
+        var index = 0
+        
+        repeat {
+            var lastIndex = index + bufSize
+            if lastIndex > data.count {
+                lastIndex = index + data.count - index
+            }
+            
+            let data2 = data[index..<lastIndex]
+            xxh.update(data2)
+            
+            index += data2.count
+            if index >= data.count {
+                break
+            }
+        } while(true)
+        
+        let hash = xxh.digest()
+        print("XXH32 Streaming -> 0x\(String(format: "%08x", hash))")
     }
-  }
-  
-  
-  // MARK: - Streaming
-  static func streamingXXH32() {
-    // Create xxHash instance
-    let xxh = XXH32() // if using seed, e.g. "XXH32(0x7fffffff)"
     
-    // Get data from file
-    let bundle = Bundle(for: Common.self)
-    let path = bundle.path(forResource: "alice29", ofType: "txt")!
-    let data = NSData(contentsOfFile: path)! as Data
+    static func streamingXXH64() {
+        // Create xxHash instance
+        let xxh = XXH64() // if using seed, e.g. "XXH64(0x000000007fffffff)"
+        
+        // Get data from file
+        let bundle = Bundle(for: Common.self)
+        let path = bundle.path(forResource: "alice29", ofType: "txt")!
+        let data = NSData(contentsOfFile: path)! as Data
+        
+        let bufSize = 1024
+        var index = 0
+        
+        repeat {
+            var lastIndex = index + bufSize
+            if lastIndex > data.count {
+                lastIndex = index + data.count - index
+            }
+            
+            let data2 = data[index..<lastIndex]
+            xxh.update(data2)
+            
+            index += data2.count
+            if index >= data.count {
+                break
+            }
+        } while(true)
+        
+        let hash = xxh.digest()
+        print("XXH64 Streaming -> 0x\(String(format: "%016lx", hash))")
+    }
     
-    let bufSize = 1024
-    var index = 0
-    
-    repeat {
-      var lastIndex = index + bufSize
-      if lastIndex > data.count {
-        lastIndex = index + data.count - index
-      }
-      
-      let data2 = data[index..<lastIndex]
-      xxh.update(data2)
-      
-      index += data2.count
-      if index >= data.count {
-        break
-      }
-    } while(true)
-    
-    let hash = xxh.digest()
-    print("XXH32 Streaming -> 0x\(String(format: "%08x", hash))")
-  }
-  
-  static func streamingXXH64() {
-    // Create xxHash instance
-    let xxh = XXH64() // if using seed, e.g. "XXH64(0x000000007fffffff)"
-    
-    // Get data from file
-    let bundle = Bundle(for: Common.self)
-    let path = bundle.path(forResource: "alice29", ofType: "txt")!
-    let data = NSData(contentsOfFile: path)! as Data
-    
-    let bufSize = 1024
-    var index = 0
-    
-    repeat {
-      var lastIndex = index + bufSize
-      if lastIndex > data.count {
-        lastIndex = index + data.count - index
-      }
-      
-      let data2 = data[index..<lastIndex]
-      xxh.update(data2)
-      
-      index += data2.count
-      if index >= data.count {
-        break
-      }
-    } while(true)
-    
-    let hash = xxh.digest()
-    print("XXH64 Streaming -> 0x\(String(format: "%016lx", hash))")
-  }
-  
 }
 
--- demo/xxHashDemo-macOS/xxHashDemo-macOS/ViewController.swift
+++ demo/xxHashDemo-macOS/xxHashDemo-macOS/ViewController.swift
@@ -11,23 +11,23 @@ import Cocoa
 import xxHash_Swift
 
 class ViewController: NSViewController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
     
-    Common.oneshotXXH32()
-    Common.oneshotXXH64()
-    Common.oneshotXXH3_64()
-    Common.oneshotXXH3_128()
-    Common.streamingXXH32()
-    Common.streamingXXH64()
-  }
-  
-  override var representedObject: Any? {
-    didSet {
-      // Update the view, if already loaded.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Common.oneshotXXH32()
+        Common.oneshotXXH64()
+        Common.oneshotXXH3_64()
+        Common.oneshotXXH3_128()
+        Common.streamingXXH32()
+        Common.streamingXXH64()
     }
-  }
-  
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
 }
 
